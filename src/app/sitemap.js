3c8dcea2a39a849cc2211ca76a24581e40922394
Nishith P Shetty <nishithpshetty@gmail.com>
2024-09-10 23:41:15 +0530

    Update sitemap.js

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -7,34 +7,28 @@ export default function sitemap() {
       priority: 1,
     },
     {
-      url: "https://www.nishithpshetty.com/about",
+      url: "https://www.nishithpshetty.com/projects",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.9,
     },
     {
-      url: "https://www.nishithpshetty.com/projects",
+      url: "https://www.nishithpshetty.com/photography",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.8,
     },
     {
-      url: "https://www.nishithpshetty.com/photography",
+      url: "https://www.nishithpshetty.com/contact",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.7,
     },
     {
-      url: "https://www.nishithpshetty.com/contact",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.6,
-    },
-    {
       url: "https://www.nishithpshetty.com/Resume.pdf",
       lastModified: new Date(),
       changeFrequency: "weekly",
-      priority: 0.5,
+      priority: 0.6,
     },
   ];
 }
