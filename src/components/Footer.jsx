98e2f20c8442ed6b2bbf6294c9c7c202e90dcac5
Nishith P Shetty <nishithpshetty@gmail.com>
2024-09-10 23:30:31 +0530

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,23 @@
+// components
+import { Socials } from "./Socials";
+
 export const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="bg-secondary py-12">
+      <div className="container mx-auto">
+        <div className="flex flex-col items-center justify-between">
+          {/* socials */}
+          <Socials
+            containerStyles="flex gap-x-6 mx-auto xl:mx-0 mb-4"
+            iconsStyles="text-primary text-[20px] dark:text-muted-foreground text-[20px] hover:text-muted-foreground dark:hover:text-primary transition-all"
+          />
+          {/* copyright */}
+          <div className="text-muted-foreground">
+            Copyright &copy; Nishith P Shetty {new Date().getFullYear()}. All
+            rights reserved.
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
